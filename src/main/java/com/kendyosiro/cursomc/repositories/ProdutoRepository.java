21cbc0a56cae239ec03d3bf74c3fbc5a8f6774c5
Kendy Osiro <kendyosiro@gmail.com>
2020-05-13 09:58:01 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/kendyosiro/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/kendyosiro/cursomc/repositories/ProdutoRepository.java
@@ -2,9 +2,9 @@ package com.kendyosiro.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.kendyosiro.cursomc.domain.Categoria;
+import com.kendyosiro.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
